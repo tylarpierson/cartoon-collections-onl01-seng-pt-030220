f0c8a106289dacca102938ea7dbaac1f929e54f0
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 20:50:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  array.map do |name| 
+    if name.length > 4 
+      true 
+    else 
+      false 
+    end 
+  end
 end
 
 def find_the_cheese# code an argument here
