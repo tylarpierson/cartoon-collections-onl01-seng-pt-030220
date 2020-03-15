f37a59c64811eb850c0328816ac0d67ae18c56e3
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 20:59:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?(cheese) 
+    puts cheese 
+  else 
+    nil 
+  end
 end
