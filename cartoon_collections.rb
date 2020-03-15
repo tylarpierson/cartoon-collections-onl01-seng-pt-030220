16c590b98763cc1693a5841ddfc62aded9e06305
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 21:04:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,6 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |type|
-    
+    cheese_types.include?(type)
+  end 
 end
